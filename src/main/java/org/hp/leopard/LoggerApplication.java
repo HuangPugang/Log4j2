c82092aa64da265a990ca6459b3227b8bfd7021f
Paul <huangpg@59store.com>
2016-12-20 17:27:14 +0800

    first commit

---
+++ src/main/java/org/hp/leopard/LoggerApplication.java
@@ -0,0 +1,12 @@
+package org.hp.leopard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerApplication.class, args);
+	}
+}
